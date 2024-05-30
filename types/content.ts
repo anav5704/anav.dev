9ca7a5bc323b95ad738d85aceb8ebf5852ecc098
Anav Chand <anavkki@gmail.com>
2024-05-30 20:45:58 +1200

    chore: add content types

---
+++ types/content.ts
@@ -0,0 +1,26 @@
+export type Project = {
+    slug: string
+    title: string
+    date: string
+    image: string,
+    content: string,
+    isFeatured: boolean
+}
+
+export type Blog = {
+    slug: string
+    title: string
+    date: string
+    image: string,
+    content: string,
+    isFeatured: boolean
+}
+
+export type Research = {
+    slug: string
+    title: string
+    date: string
+    image: string,
+    content: string,
+    isFeatured: boolean
+}
