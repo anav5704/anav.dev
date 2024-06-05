47b7de1f1a0767ff80781ae502843f402752ee67
Anav Chand <anavkki@gmail.com>
2024-06-05 19:21:41 +1200

    feat: new hero section

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,8 +5,14 @@ export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
     theme: {
         extend: {
+            colors: {
+                "primary-dim": "var(--primary-dim)",
+                "primary-accent": "var(--primary-accent)",
+                "primary-bright": "var(--primary-bright)",
+            },
             fontFamily: {
-                zodiak: ["Zodiak", defaultTheme.fontFamily.serif],
+                satoshi: ["Satoshi", defaultTheme.fontFamily.sans],
+                dmSans: ["DMSans", defaultTheme.fontFamily.sans],
                 jakatra: ["Jakatra", defaultTheme.fontFamily.serif]
             }
         },
