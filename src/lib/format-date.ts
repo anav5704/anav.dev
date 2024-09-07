cf65577723e308a80f93cefb31ed5ace13f01a82
Anav Chand <anavkki@gmail.com>
2024-09-07 19:15:31 +1200

    chore: updage images

--- src/lib/format-date.ts
+++ src/lib/format-date.ts
@@ -1,5 +1,5 @@
-import { format } from "date-fns";
+import { format } from "date-fns"
 
 export const formatDate = (date: Date) => {
-    return format(date, "PPP");
-};
+    return format(date, "PPP")
+}
