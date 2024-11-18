0e5ad9ccca85e97f65cc18fc331f28890809c939
Anav Chand <anavkki@gmail.com>
2024-11-18 16:32:23 +1200

    build: add rehype-mathjax

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,31 +1,30 @@
 import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
+import rehypeMathjax from 'rehype-mathjax'
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import gruvbox from './gruvbox.json'
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
     site: "https://www.anav.dev",
     output: "hybrid",
-    adapter: vercel({
-        webAnalytics: {
-            enabled: true
-        }
-    }),
+    adapter: vercel(),
     prefetch: {
         prefetchAll: true,
         defaultStrategy: "viewport"
     },
-    devToolbar: {
-        enabled: false
-    },
     markdown: {
         syntaxHighlight: "shiki",
         shikiConfig: {
-            theme: gruvbox
-        }
+            theme: gruvbox,
+        },
+        rehypePlugins: [
+            rehypeMathjax
+        ],
     },
-    integrations: [tailwind(), react(), sitemap()]
+    integrations: [tailwind(), react(), sitemap()],
+    devToolbar: {
+        enabled: false
+    },
 });
