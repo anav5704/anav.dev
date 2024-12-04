b6387381e129bb1b81eeece713d3d0d492d84dd1
Anav Chand <anavkki@gmail.com>
2024-12-04 16:25:53 +1200

    refactor: create long and short date formatters

--- src/lib/format-date.ts
+++ src/lib/format-date.ts
@@ -1,5 +1,9 @@
 import { format } from "date-fns"
 
-export const formatDate = (date: Date) => {
+export const longDate = (date: Date) => {
     return format(date.toLocaleDateString("en-US", { timeZone: "Pacific/Fiji" }), "PPP")
+}
+
+export const shortDate = (date: Date) => {
+    return format(date.toLocaleDateString("en-US", { timeZone: "Pacific/Fiji" }), "MMM yyyy")
 }
