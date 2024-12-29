93a5d6d9c3813285f6a7204eb0a9d2c17219432e
Anav Chand <anavkki@gmail.com>
2024-12-29 23:10:13 +1200

    build: migrate from vercel to cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
+import cloudflare from "@astrojs/cloudflare";
 import { defineConfig } from "astro/config";
 import rehypeMathjax from 'rehype-mathjax'
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import vercel from "@astrojs/vercel";
 import gruvbox from './gruvbox.json'
 import react from "@astrojs/react";
+
 
 export default defineConfig({
     site: "https://www.anav.dev",
     output: "static",
-    adapter: vercel(),
+    adapter: cloudflare(),
     prefetch: {
         prefetchAll: true,
         defaultStrategy: "viewport"
