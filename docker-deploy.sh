0a70b82aa20022a4df9f528463d0537a78b177c8
Anav Chand <anavkki@gmail.com>
2025-01-02 19:22:38 +1200

    build: simplify docker deploy script

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -2,18 +2,7 @@
 
 set -eu
 
-DOCKER_USER=$1
-DOCKER_PASSWD=$2
-IMAGE_NAME=$3
-
-docker_login() {
-    echo "Logging into Docker..."
-
-    echo $DOCKER_PASSWD | docker login -u $DOCKER_USER --password-stdin || {
-        echo "Failed to login to Docker"
-        exit 1
-    }
-}
+IMAGE_NAME=$1
 
 clear_containers() {
     echo "Clearing containers..."
@@ -25,14 +14,15 @@ clear_containers() {
 start_container () {
     echo "Starting container..."
 
-    docker run -d -p 4321:4321 $IMAGE_NAME:latest || {
+    OPTIONS="-d --restart unless-stopped -p 4321:4321 --env-file .env.production"
+
+    docker run $OPTIONS $IMAGE_NAME:latest || {
         echo "Failed to start container"
         exit 1
     }
 }
 
 docker_deploy () {
-    docker_login
     clear_containers
    start_container 
 }
