4e101e317633502d7c224fbdba4e361d96bd4877
Anav Chand <anavkki@gmail.com>
2025-01-03 18:37:24 +1200

    chore: test env vars

--- src/drizzle/index.ts
+++ src/drizzle/index.ts
@@ -2,6 +2,14 @@ import { drizzle } from "drizzle-orm/postgres-js"
 import * as schema from "@drizzle/schema"
 import postgres from "postgres"
 
-const client = postgres(process.env.DATABASE_URL as string)
+const url = import.meta.env.PROD ? process.env.DATABASE_URL : import.meta.env.DATABASE_URL
+
+console.log("Node", process.env.DATABASE_URL)
+
+console.log("Astro", import.meta.env.DATABASE_URL)
+
+const client = postgres(url, {
+    debug: true
+})
 
 export const db = drizzle(client, { schema })
