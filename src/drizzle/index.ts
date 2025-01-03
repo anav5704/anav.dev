62e65a029e3f829992160f2f84a7153984d060fa
Anav Chand <anavkki@gmail.com>
2025-01-03 18:06:38 +1200

    fix: pass database url using node process

--- src/drizzle/index.ts
+++ src/drizzle/index.ts
@@ -2,6 +2,6 @@ import { drizzle } from "drizzle-orm/postgres-js"
 import * as schema from "@drizzle/schema"
 import postgres from "postgres"
 
-const client = postgres(import.meta.env.DATABASE_URL)
+const client = postgres(process.env.DATABASE_URL as string)
 
 export const db = drizzle(client, { schema })
