6da7107c7bd89aa0033e9aa9fc13a74a1a4f465c
Anav Chand <anavkki@gmail.com>
2025-02-17 09:39:54 +1200

    chore: move get date fn from lib to utils

--- src/utils/getDate.ts
+++ src/utils/getDate.ts
@@ -1,16 +1,16 @@
-export const longDate = (date: Date) => {
+export const long = (date: Date) => {
     return date.toLocaleDateString("en-US", {
         timeZone: "Pacific/Fiji",
         month: "long",
         day: "numeric",
         year: "numeric"
-    })
-}
+    });
+};
 
-export const shortDate = (date: Date) => {
+export const short = (date: Date) => {
     return date.toLocaleDateString("en-US", {
         timeZone: "Pacific/Fiji",
         month: "short",
         year: "numeric"
-    })
-}
+    });
+};
