76cd6989b283f86c2050749bed417b7337baa170
anav5704 <anavkki@gmail.com>
2025-03-09 13:20:21 +1300

    chore: bump astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
 import partytown from "@astrojs/partytown";
-import links from "rehype-external-links"
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import gruvbox from "./gruvbox.json";
 import latex from "rehype-mathjax";
 import node from "@astrojs/node";
 import d2 from "astro-d2";
-
 
 export default defineConfig({
     site: "https://www.anav.dev",
@@ -27,26 +25,17 @@ export default defineConfig({
         syntaxHighlight: "shiki",
         shikiConfig: {
             theme: gruvbox,
-            // langs: []
         },
         rehypePlugins: [
-            [links, {
-                target: "_blank"
-            }],
             latex,
         ],
     },
 
-    integrations: [
-        d2({
-            pad: 0,
-            theme: {
-                default: "301",
-                dark: false
-            }
-        }),
-        tailwind(),
-        partytown(),
-        sitemap()
-    ],
+    integrations: [d2({
+        pad: 0,
+        theme: {
+            default: "301",
+            dark: false
+        }
+    }), tailwind(), partytown(), sitemap()],
 });
