744cf73fdfbed8a02369f1dfa613a367f2f05d4d
Anav Chand <anavkki@gmail.com>
2025-07-27 21:16:02 +1200

    feat: add copy url component

---
+++ src/components/CopyUrl.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+    import { Link, Check } from "@lucide/svelte";
+
+    const { label, url } = $props();
+
+    let isCopied = $state(false);
+
+    const handleClick = () => {
+        isCopied = true;
+
+        navigator.clipboard.writeText(url);
+
+        setTimeout(() => {
+            isCopied = false;
+        }, 1500);
+    };
+</script>
+
+<button onclick={handleClick} class="flex items-center gap-1">
+    {#if isCopied}
+        <Check size={20} />
+        Link Copied
+    {:else}
+        <Link size={20} />
+        <p>{label}</p>
+    {/if}
+</button>
